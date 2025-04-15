5b762f9053a8b357fd71ca3032a6a378409d1d25
Hoang-DangUNSW <z5594450@ad.unsw.edu.au>
2025-04-15 23:27:22 +1000

    add object interfaces in type.ts based on the google doc

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -5,8 +5,22 @@ export interface User {
     userId: string;
     username: string;
     password: string;
+    topAlbums: [Album];
+    reviews: [ObjectId]; 
+    friends: [string]; //list of userIds
+
 };
+
+export interface Album {
+    name: string; 
+    artist: string; 
+    coverImage?: string;  
+}
 
 export interface Review {
     _id: ObjectId;
+    ownerId: string; 
+    album: Album;
+    rating: string; 
+    creationDate: number; 
 };
